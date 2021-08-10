cda9e54609c16051ed25ced7b034081393b64c9a
seulguo <rowoom@naver.com>
2021-08-10 13:42:08 +0900

    2-1.ㅔㅛ

---
+++ Inflearn/Algorithm/2-1.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+a = list()
+for i in range(N):
+    if N % (i+1) == 0:
+        a.append(i+1)
+print(a[K-1])
+
+'''
+인프런 파이썬 알고리즘 문제풀이, 2-1 K번째 약수
+문제 출처 : 한국정보올림피아드 문제
+'''
